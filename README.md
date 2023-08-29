6b363addfbfcad07e828df91b7dd98cb95bd067c
Sarat Chandra <me@saratchandra.in>
2023-08-29 14:30:11 +0530

    feat: Add support for custom date format.

--- README.md
+++ README.md
@@ -1,15 +1,46 @@
-# xirrcalc
-A CLI for calculating XIRR from a CSV file.
+# XIRRCalc
 
+XIRRCalc is a command-line interface tool designed to calculate the XIRR (Extended Internal Rate of Return) from cash flow data stored in a CSV file.
+
+## Table of Contents
+
+- [Features](#features)
+- [Usage](#usage)
+- [CSV File Format](#csv-file-format)
+- [Support & Contribution](#support--contribution)
+- [License](#license)
+
+## Features
+
+- Quick calculation of XIRR from a CSV file.
+- Ability to specify custom date formats.
+- Flexibility in date formatting with default support for `yyyy-mm-dd` and `dd/mm/yy`.
 
 ## Usage
 
 ```bash
-$ ./xirrcalc cashflows.csv
+$ ./xirrcalc [path-to-cashflows.csv] [date-format]
 ```
 
-## CSV Format
+#### Arguments:
 
-The csv containing the cashflows has to be in the format of `amount,date`.
+- [path-to-cashflows.csv] : The path to your CSV file containing the cash flows.
+- [date-format] (optional) : Date format of the entries in your CSV. If not provided, the tool will try to recognize either yyyy-mm-dd or dd/mm/yy formats.
 
-Date can be in the format of either `yyyy-mm-dd` or `dd/mm/yy`.
+## CSV File Format
+
+Ensure your CSV file conforms to the following format:
+
+```
+amount,date
+1000,01/01/2022
+-500,06/01/2022
+```
+
+## Support & Contribution
+
+For any issues or feature requests, please open an issue or submit a pull request. Contributions are always welcome!
+
+## License
+
+This project is licensed under the MIT License. See the LICENSE file for details.
--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,67 @@
-use std::fs::File;
-use std::{process, ffi::OsString};
+use chrono::NaiveDate;
 use std::env;
 use std::error::Error;
-use chrono::ParseError;
-use xirr::*;
+use std::fs::File;
+use std::process;
+use xirr::Payment;
 
 fn main() {
     if let Err(err) = run() {
-        println!("error reading csv: {}", err);
+        eprintln!("error: {}", err);
         process::exit(1);
     }
 }
 
 fn run() -> Result<(), Box<dyn Error>> {
-    let mut pmnts: Vec<Payment> = vec![];
-    let file_path = get_first_arg()?;
-    let file = File::open(file_path)?;
-    let mut rdr = csv::ReaderBuilder::new().has_headers(false).from_reader(file);
-    for result in rdr.records() {
-        let record = result?;
-        let date: Result<chrono::NaiveDate, ParseError> = match chrono::NaiveDate::parse_from_str(&record[1], "%d/%m/%y") {
-            Ok(date) => Ok(date),
-            Err(_) => {
-                // Try the other format as well.
-                let d = chrono::NaiveDate::parse_from_str(&record[1], "%Y-%m-%d")?;
-                Ok(d)
-            }
-        };
-        
-        let payment = Payment{
-            amount: record[0].parse().unwrap(),
-            date: date?
-        };
-        println!("payment: {}, date: {}", payment.amount, payment.date);
-        pmnts.push(payment);
-    }
-    let xirr = compute(&pmnts).unwrap();
-    println!("xirr is: {}%", xirr * 100.0);
+    let file_path = get_file_path()?;
+    let pmnts = read_payments_from_file(&file_path)?;
+    let xirr = xirr::compute(&pmnts)?;
+    println!("xirr is: {:.2}%", xirr * 100.0);
     Ok(())
 }
 
-fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
-    match env::args_os().nth(1) {
-        None => Err(From::from("expected 1 argument, but got none")),
-        Some(file_path) => Ok(file_path),
+fn get_file_path() -> Result<String, Box<dyn Error>> {
+    env::args()
+        .nth(1)
+        .ok_or_else(|| From::from("expected 1 argument, but got none"))
+}
+
+fn read_payments_from_file(file_path: &str) -> Result<Vec<Payment>, Box<dyn Error>> {
+    let file = File::open(file_path)?;
+    let mut rdr = csv::ReaderBuilder::new()
+        .has_headers(false)
+        .from_reader(file);
+
+    let dfmts = get_dfmts();
+    let mut pmnts = vec![];
+
+    for result in rdr.records() {
+        let record = result?;
+        let date = get_date(&record[1], &dfmts)?;
+        let amount = record[0].parse::<f64>()?;
+        pmnts.push(Payment { amount, date });
+        println!("payment: {}, date: {}", amount, date);
     }
+
+    Ok(pmnts)
+}
+
+fn get_dfmts() -> Vec<String> {
+    match env::args().nth(2) {
+        Some(dfmt) => vec![dfmt],
+        None => vec![
+            "%d/%m/%y".to_string(),
+            "%d/%m/%Y".to_string(),
+            "%Y-%m-%d".to_string(),
+        ],
+    }
+}
+
+fn get_date(date: &str, dfmts: &[String]) -> Result<NaiveDate, Box<dyn Error>> {
+    for fmt in dfmts {
+        if let Ok(parsed_date) = NaiveDate::parse_from_str(date, fmt) {
+            return Ok(parsed_date);
+        }
+    }
+    Err(From::from("invalid date"))
 }
